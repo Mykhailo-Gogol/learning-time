4576a2f6cbbac01be9b876f7264418b333076948
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2020-11-06 14:31:40 +0200

    Додав карту GoogleMaps embed

--- script.js
+++ script.js
@@ -65,3 +65,89 @@
 // console.log(poly.sell("Microwave"));
 // console.log(poly.sell("Mac Book")); // Manager Poly sold Microwave
 // console.log(poly.sales); // 12
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// const animal = { eats: true };
+// const dog = Object.create(animal);
+// dog.barks = true;
+
+// console.log(dog.barks); // true
+// console.log(dog.eats); // true
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// const animal = { eats: true };
+// const dog = Object.create(animal);
+// dog.barks = true;
+
+// for (const key in dog) {
+//   console.log(key); // barks, eats
+// }
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// const animal = { eats: true };
+// const dog = Object.create(animal);
+// dog.barks = true;
+
+// for (const key in dog) {
+//   if (!dog.hasOwnProperty(key)) continue;
+
+//   console.log(key); // barks
+// }
+
+// const animal = { eats: true };
+// const dog = Object.create(animal);
+// dog.barks = true;
+
+// const dogKeys = Object.keys(dog);
+
+// console.log(dogKeys); // ['barks']
+
+// for (const key in dog) {
+//   console.log(dog.hasOwnProperty(key)); // barks, eats
+// }
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// const Guest = function (name, room) {
+//   this.name = name;
+//   this.room = room;
+// };
+
+// Guest.prototype.showGuestInfo = function () {
+//   console.log(`name: ${this.name}, room: ${this.room}`);
+// };
+
+// Guest.prototype.sayHello = function () {
+//   console.log(`Hello, ${this.name}. Your order: room ${this.room}`);
+// };
+
+// const mango = new Guest("Mango", 28);
+// const poly = new Guest("Poly", 36);
+
+// mango.showGuestInfo(); // name: Mango, room: 28
+// poly.showGuestInfo(); // name: Poly, room: 36
+// mango.sayHello();
+// poly.sayHello();
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// const Hero = function (name, xp) {
+//   this.name = name;
+//   this.xp = xp;
+// };
+
+// /*
+//  * Теперь у нас есть конструктор базового класса героя,
+//  * добавим в prototype какой-то метод.
+//  */
+// Hero.prototype.gainXp = function (amount) {
+//   console.log(`${this.name} gained ${amount} experience points`);
+//   this.xp += amount;
+// };
+
+// const mango = new Hero("Mango", 1000);
+// console.log(mango); // Hero { name: 'Mango', xp: 1000 }
+
+// // Так как mango это экземпляр Hero, то ему доступны методы из Hero.prototype
+// mango.gainXp(600); // Mango gained 500 experience points
+// console.log(mango); // Hero { name: 'Mango', xp: 1500 }
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
