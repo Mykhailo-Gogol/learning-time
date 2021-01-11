3e396d551496e4ccd5e7a741f5043a859717b5d1
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2021-01-12 00:43:57 +0200

    Додав gh-pages deploy

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
+    publicPath: "/learning-time/",
   },
   devServer: {
     contentBase: path.join(__dirname, "build"),
